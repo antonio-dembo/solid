c0649ea1e80368fc2632fb99c27654680778ac8b
beboantonio <beboantonio1@hotmail.com>
2021-08-25 00:32:58 -0300

    add correction by deviding IOrder interface

--- InterfaceSegregationPrinciple/CodeWithISPViolated.cs
+++ InterfaceSegregationPrinciple/CodeWithISPViolated.cs
@@ -1,12 +1,17 @@
 namespace InterfaceSegregationPrinciple
 {
+    //We can resolve this violation by dividing IOrder Interface.
     public interface IOrder
     {
-        void AddToCart();
+        void AddToCart();        
+    }
+
+    public interface IOnlineOrder
+    {
         void CCProcess();
     }
 
-    public class OnlineOrder : IOrder
+    public class OnlineOrder : IOrder , IOnlineOrder
     {
         public void AddToCart()
         {
@@ -25,12 +30,7 @@ namespace InterfaceSegregationPrinciple
         {
             // Do add product to cart
         }
-
-        public void CCProcess()
-        {
-            // no required for Cash , offline Order
-            throw new System.NotImplementedException();
-        }
+        
     }
 
 }
